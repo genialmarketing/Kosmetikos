e8d6cff6d30195388f508ec5f0216c1c31319b46
genialmarketing <genialmarketingsales@gmail.com>
2026-07-25 18:32:01 +0200

    Add files via upload

---
+++ _serve.ps1
@@ -0,0 +1,50 @@
+$root = Split-Path -Parent $MyInvocation.MyCommand.Path
+$port = 8791
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:$port/")
+$listener.Start()
+Write-Host "Serving $root on http://localhost:$port/"
+
+$mime = @{
+  ".html" = "text/html; charset=utf-8"
+  ".css"  = "text/css"
+  ".js"   = "application/javascript"
+  ".png"  = "image/png"
+  ".jpg"  = "image/jpeg"
+  ".svg"  = "image/svg+xml"
+  ".xml"  = "application/xml"
+  ".txt"  = "text/plain"
+  ".ico"  = "image/x-icon"
+}
+
+while ($listener.IsListening) {
+  $ctx = $listener.GetContext()
+  $req = $ctx.Request
+  $res = $ctx.Response
+  try {
+    $path = [System.Uri]::UnescapeDataString($req.Url.AbsolutePath)
+    if ($path -eq "/") { $path = "/index.html" }
+    $filePath = Join-Path $root ($path.TrimStart('/'))
+    if (Test-Path $filePath -PathType Container) {
+      $filePath = Join-Path $filePath "index.html"
+    }
+    if (Test-Path $filePath -PathType Leaf) {
+      $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+      $ct = $mime[$ext]
+      if (-not $ct) { $ct = "application/octet-stream" }
+      $bytes = [System.IO.File]::ReadAllBytes($filePath)
+      $res.ContentType = $ct
+      $res.Headers.Add("Cache-Control", "no-store, no-cache, must-revalidate")
+      $res.ContentLength64 = $bytes.Length
+      $res.OutputStream.Write($bytes, 0, $bytes.Length)
+    } else {
+      $res.StatusCode = 404
+      $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found: $path")
+      $res.OutputStream.Write($msg, 0, $msg.Length)
+    }
+  } catch {
+    $res.StatusCode = 500
+  } finally {
+    $res.OutputStream.Close()
+  }
+}
